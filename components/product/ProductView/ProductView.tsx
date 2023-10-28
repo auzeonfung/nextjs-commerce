07aefaaef95ce46da9fecb65d5160fcb5d54ed33
ox <github@ox.ci>
2023-10-28 06:37:30 +0000

    npx @next/codemod@latest next-image-to-legacy-image .

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames'
-import Image from 'next/image'
+import Image from "next/legacy/image"
 import { NextSeo } from 'next-seo'
 import s from './ProductView.module.css'
 import { FC } from 'react'
